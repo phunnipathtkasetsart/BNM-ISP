40584da5de76fd4a58b4da31b89f9849af2627f8
Khun Shu <jjdhere48@gmail.com>
2026-09-08 10:37:02 +0700

    Point wsgi.py at the renamed academic_hub settings module

--- source/academic_hub/academic_hub/wsgi.py
+++ source/academic_hub/academic_hub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name_list.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academic_hub.settings')
 
 application = get_wsgi_application()
